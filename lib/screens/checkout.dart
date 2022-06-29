46b874d76b7e6df6825eb9240fb1703784e26aec
Ayodeji Olabisi <yinka.olabisi15@gmail.com>
2022-06-29 15:47:13 +0100

    STEP 4: Import the necessary packages

--- lib/screens/checkout.dart
+++ lib/screens/checkout.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:flutter_paystack/flutter_paystack.dart';
 
 class CheckOutPage extends StatefulWidget {
   const CheckOutPage({Key? key}) : super(key: key);
