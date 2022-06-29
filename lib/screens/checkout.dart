f25dd755eac705b0dc2bc4a7a568a9e4c6ea0f8a
Ayodeji Olabisi <yinka.olabisi15@gmail.com>
2022-06-29 15:42:38 +0100

    Step 3: Create your payment page and card

---
+++ lib/screens/checkout.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+
+class CheckOutPage extends StatefulWidget {
+  const CheckOutPage({Key? key}) : super(key: key);
+
+  @override
+  State<CheckOutPage> createState() => _CheckOutPageState();
+}
+
+class _CheckOutPageState extends State<CheckOutPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'Paystack Demo',
+          style: TextStyle(fontWeight: FontWeight.bold),
+        ),
+        centerTitle: true,
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          CheckOutCard(),
+        ],
+      ),
+    );
+  }
+
+  Widget CheckOutCard() {
+    return InkWell(
+      onTap: () {},
+      child: Card(
+        margin: EdgeInsets.symmetric(horizontal: 10),
+        color: Colors.blueAccent,
+        elevation: 15,
+        child: Container(
+          height: 100,
+          child: Padding(
+            padding: const EdgeInsets.all(32.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                Text(
+                  "Pay with Paystack",
+                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                ),
+                Icon(
+                  Icons.payment_rounded,
+                  size: 30,
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
